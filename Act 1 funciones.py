ae658034ee5f203f0b7dc82800cc50bd531b58e8
matias <mmendietacoggiola@gmail.com>
2025-09-17 14:36:02 -0300

    commit inicial

---
+++ Act 1 funciones.py
@@ -0,0 +1,3 @@
+def calcular_densidad(masa, volumen):
+    densidad = masa / volumen
+    return densidad
---
+++ main.py
@@ -0,0 +1,19 @@
+# Historia 2
+# La [serie] sigue la historia de [Johnny Lawrence], exestudiante del dojo [Cobra Kai],
+# quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,
+# su rivalidad con un ahora exitoso [Daniel LaRusso].
+# Este [enemigo] ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor Miyagi.
+# La serie trata sobre dos hombres que enfrentan sus demonios del pasado y
+# resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.
+
+peliserie= str(input("ingrese si es serie o pelicula: "))
+nombre1=str(input("ingrese un nombre: "))
+nombre2=str(input("ingrese otro nombre: "))
+nombredojo=str(input("ingrese nombre del dojo: "))
+marce=str(input("poner marce: "))
+
+print=("La "+peliserie+" sigue la historia de "+nombre1+", exestudiante del dojo "+nombredojo+"")
+print=("quien busca la redención reabriendo el infame dojo y reavivando, en el proceso,")
+print=("su rivalidad con un ahora exitoso "+nombre2+".Este "+marce+" ha estado luchando por mantener el equilibrio en su vida sin la guía de su mentor, el señor Miyagi")
+print=("La serie trata sobre dos hombres que enfrentan sus demonios del pasado y")
+print=("resuelven las frustraciones del presente de la única manera en que saben hacerlo: a través del karate.")
